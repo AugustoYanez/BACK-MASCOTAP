66671756dfd12026c1c8f3f6be84226863139198
augustoyanez <augustocasla01@gmail.com>
2024-09-09 11:29:20 -0300

    REQUERIMIENTOS DE SEGURIDAD ANDA TODO

--- src/models/auth.models.ts
+++ src/models/auth.models.ts
@@ -3,63 +3,64 @@ import { Estado, Documento, Contacto, Rol } from "../interfaces/enums";
 import { IUsuario } from "../interfaces/Usuario";
 import { IMascota } from "../interfaces/Mascota";
 
+const usuarioSchema = new mongoose.Schema({
+  nombre: {
+      type: String,
+      required: true,
+      trim: true
+  },
+  apellido: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+  },
+  contrasena: {
+      type: String,
+      required: true,
+      minlength: 8,
+      trim: true
+  },
+  nroDocumento: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  telefono: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  documento: {
+      type: String,
+      enum: Object.values(Documento),
+      required: true,
+  },
+  contacto: {
+      type: String,
+      enum: Object.values(Contacto),
+      required: true,
+  },
+  rol: {
+      type: String,
+      enum: Object.values(Rol)
+  },
+  mascotas: [
+    { 
+      type: mongoose.Schema.Types.ObjectId, ref: 'Mascota' 
+    }
+  ],
+  intentosFallidos: { type: Number, default: 0 },
+  bloqueadoHasta: { type: Number, default: undefined } 
+});
 
-const usuarioSchema = new mongoose.Schema<IUsuario>({
-    nombre: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    apellido: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    contrasena: {
-        type: String,
-        required: true,
-        minlength: 8,
-        trim: true
-    },
-    nroDocumento: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    telefono: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    documento: {
-        type: String,
-        enum: Object.values(Documento),
-        required: true,
-    },
-    contacto: {
-        type: String,
-        eunm: Object.values(Contacto),
-        required: true,
-    },
-    rol: {
-        type: String,
-        enum: Object.values(Rol)
-    },
-    mascotas: [
-      { 
-        type: mongoose.Schema.Types.ObjectId, ref: 'Mascota' 
-      }
-    ]
-})
-
-export const Usuario = mongoose.model('Usuario', usuarioSchema)
+export const Usuario = mongoose.model('Usuario', usuarioSchema);
 
 
 const mascotaSchema = new mongoose.Schema<IMascota>({
